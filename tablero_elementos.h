1037095b5bdf7c6e35883b8eeccfc6e22b07c369
AngelFC89 <61050068+AngelFC89@users.noreply.github.com>
2020-04-29 10:19:56 +0200

    Create tablero_elementos.h

---
+++ tablero_elementos.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+ 
+#define DIMTAB_X 63			//Dimensión del tablero(impresión).
+#define DIMTAB_Y 146		//Dimensión del tablero(impresión).
+#define DIMTAB_REL 10		//Dimensión del tablero relativa a las casillas.
+#define DFIL 6 				//Desplazamiento de fila.
+#define DCOL 14 			//Desplazamiento de columnas.
+#define ZFIL 55 			//'Cero' de las filas.
+#define ZCOL 5				//'Cero' de las columnas.
+#define MFIL 7 				//Máximo de las filas.
+#define MCOL 117 			//Maximo de las columnas. 
+#define DIM_PIEZASJ 8		//Dimensión arreglo de piezas.(8 piezas por jugador).
+
+#define DIRTAB "prTabV2.txt" //Dirección del archivo del tablero.(*INNECESARIO AQUÍ YA QUE SOLO LO LLAMAMOS UNA VEZ*)
+
+
+typedef struct {
+	
+	int x;	//coordenada fila.
+	int y;	//coordenada columna.
+}coord;
+
+typedef struct {
+	
+	char dFigura[5][DCOL];	//Dibujo de la pieza.
+//	jugadores pertenencia;
+//	int pertenencia;
+	coord cActual;			//Cordenadas absolutas(Dibujo).
+	coord relativas;		//Coordenadas relativas al tablero de juego.
+}pieza;
