11efc80e6d20ed618877ccc5540b374fee46a847
AngelFC89 <61050068+AngelFC89@users.noreply.github.com>
2020-05-01 13:04:20 +0200

    Create menuFMvsc.h
    Añadido el archivo de cabecera del menu

---
+++ menuFMvsc.h
@@ -0,0 +1,23 @@
+// Cabecera de Menu Fiesta de los muertos.
+
+//Prototipo de menu utilizando switch-case.
+
+//Distribucion:
+//	
+//	Menu principal:
+//		1 Nueva Partida:
+//			1.1 Nueva Combinacion:
+//				1.1.1 Combinacion Basica.
+//				1.1.2 Combinacion Media:
+//					1.1.2.1 Normal.
+//					1.1.2.2 Vista.
+//					1.1.2.3 Aleatoria.
+//				1.1.3 Combinacion Avanzada.
+//			1.2 Combinacion Guardada.
+//		2 Continuar Partida.
+//		3 Ver Estadisticas.
+//		4 Salir.
+
+#include "control.h"
+
+void menuPrincipalSC(int, tab*, pieza*, pieza*, _Bool*, _Bool*);
