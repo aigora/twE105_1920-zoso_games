26116dadf3dfc757f01b707fcd8ff072c21b3b85
DiegoBastida <d.bastida@alumnos.upm.es>
2020-05-06 11:56:29 +0200

    _Bool checkCoordP
    He subido _bool checkCoordP

--- control.h
+++ control.h
@@ -5,7 +5,7 @@
 
 #include "tablero_elementos.h"
 
-
+_Bool checkCoordP(pieza*, pieza*, coord, int*);
 int nDistMov(pieza*, pieza*, pieza*, coord);
 _Bool checkCoordC(pieza*, pieza*,pieza*, pieza*, coord, coord*);
 _Bool checkMovimiento( _Bool [][DIMTAB_REL], _Bool [][DIMTAB_REL], int , int , coord, int);
