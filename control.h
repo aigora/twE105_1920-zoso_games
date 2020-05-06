6752154ee113a8f4c48a89d243877d4a1dc1a4ba
DavidBrianes <d.brianes@alumnos.upm.es>
2020-05-06 11:40:44 +0200

    Update control.h
    he añadido _BoolfinTurno y _BoolcheckCoordC

--- control.h
+++ control.h
@@ -7,7 +7,9 @@
 
 
 int nDistMov(pieza*, pieza*, pieza*, coord);
+_Bool checkCoordC(pieza*, pieza*,pieza*, pieza*, coord, coord*);
 _Bool checkMovimiento( _Bool [][DIMTAB_REL], _Bool [][DIMTAB_REL], int , int , coord, int);
 void iniTrack( _Bool [][DIMTAB_REL]);
 void setDirecciones(coord, coord, int*, int*);
 _Bool checkPiezaMeta(pieza*, pieza*, pieza*, coord*);
+_Bool finTurno();
