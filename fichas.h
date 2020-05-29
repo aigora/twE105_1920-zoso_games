fb000b3a2f2af917602924b5ef7fa06ba8047487
AngelFC89 <61050068+AngelFC89@users.noreply.github.com>
2020-05-29 22:08:03 +0200

    Creacion de fichas.h
    Cabecera con posiciones

---
+++ fichas.h
@@ -0,0 +1,93 @@
+// Cabecera fichero fichas.
+
+// Posiciones inciales de las fichas del jugador 1.
+#define POS1J1_X 49 	//Fila
+#define POS1J1_Y 47		//Columna
+#define POS2J1_X 49
+#define POS2J1_Y 61
+#define POS3J1_X 49
+#define POS3J1_Y 75
+#define POS4J1_X 55
+#define POS4J1_Y 33
+#define POS5J1_X 55		
+#define POS5J1_Y 47
+#define POS6J1_X 55
+#define POS6J1_Y 61
+#define POS7J1_X 55
+#define POS7J1_Y 75
+#define POS8J1_X 55
+#define POS8J1_Y 89
+
+// Posiciones inicales de las fichas del jugador 2.
+#define POS1J2_X 25
+#define POS1J2_Y 19
+#define POS2J2_X 31
+#define POS2J2_Y 19
+#define POS3J2_X 37
+#define POS3J2_Y 19
+#define POS4J2_X 19
+#define POS4J2_Y 5
+#define POS5J2_X 25
+#define POS5J2_Y 5
+#define POS6J2_X 31
+#define POS6J2_Y 5
+#define POS7J2_X 37
+#define POS7J2_Y 5 
+#define POS8J2_X 43
+#define POS8J2_Y 5
+
+//Direcciones de los ficheros:
+#define DIRPEONJ1 "fPeonJ1.txt"
+#define DIRPEONJ2 "fPeonJ2.txt"
+
+// Definicion de coordenadas en el tablero que no se pueden jugar.
+// ***(Se puede reducir disminuyendo un poco la legibilidad de las variables)***
+#define A1_X 55
+#define A1_Y 5
+#define A2_X 49
+#define A2_Y 5
+#define A8_X 13
+#define A8_Y 5
+#define A9_X 7 
+#define A9_Y 5
+#define A10_X 1
+#define A10_Y 5
+#define B1_X 55
+#define B1_Y 19
+#define B9_X 7
+#define B9_Y 19
+#define B10_X 1
+#define B10_Y 19
+#define H1_X 55
+#define H1_Y 103
+#define H9_X 7
+#define H9_Y 103
+#define H10_X 1
+#define H10_Y 103
+#define I1_X 55
+#define I1_Y 117
+#define I2_X 49
+#define I2_Y 117
+#define I8_X 13
+#define I8_Y 117
+#define I9_X 7
+#define I9_Y 117
+#define I10_X 1
+#define I10_Y 117
+#define J1_X 55
+#define J1_Y 131
+#define J2_X 49
+#define J2_Y 131
+#define J8_X 13
+#define J8_Y 131
+#define J9_X 7
+#define J9_Y 131
+#define J10_X 1
+#define J10_Y 131
+// Incluimos el centro y las 2 casillas.
+#define CENTRO_X 31
+#define CENTRO_Y 61
+#define EX1_X 25
+#define EX1_Y 47
+#define EX2_X 37
+#define EX2_Y 75
