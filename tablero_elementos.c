3356f57d3647a6409bc498ea764854661a05ba8a
AngelFC89 <61050068+AngelFC89@users.noreply.github.com>
2020-05-29 22:19:55 +0200

    Actualizacion tablero_elementos
    Se han añadido funciones de guardado y cargado de partidas

--- tablero_elementos.c
+++ tablero_elementos.c
@@ -1,8 +1,22 @@
 //tablero v3.0
 
-
-#include <stdio.h>
 #include "tablero_elementos.h"
+
+
+void setCoordIni( coord *cIniJ1, coord *cIniJ2 ){
+	
+	int i;
+	
+	FILE *pf;
+	pf= fopen ("CoordIni.txt", "r");
+	
+	for ( i = 0; i < DIM_PIEZASJ; i++){
+		
+		fscanf(pf, "%d,%d\t%d,%d\n", &cIniJ1[i].x, &cIniJ1[i].y, &cIniJ2[i].x, &cIniJ2[i].y);
+	}
+	
+	fclose(pf);
+}
 
 void iniTablero(tab *pTablero, char dir[]){
 	
@@ -30,9 +44,16 @@ void iniTablero(tab *pTablero, char dir[]){
 			pTablero->coordRelativas[i][j] = 0;
 		}
 	}	
+	
+	//Añadimos el centro.
+	pTablero->coordRelativas[5][4] = 1;
 }
 
 void setTablero (tab *pTab, char ficha[][DCOL], int coordX, int coordY){
+	
+	
+	coord temp;
+	coord tempRel;
 	
 	int i,j;
 	int m =0;
@@ -49,11 +70,15 @@ void setTablero (tab *pTab, char ficha[][DCOL], int coordX, int coordY){
 		k++;
 	}
 	
-//	i = toRelX(coordX);
-//	j = toRelY(coordY);
+	temp.x = coordX;
+	temp.y = coordY;
 	
-	pTab->coordRelativas[i][j] = 1;	
+	tempRel = toRel(temp);
+	
+	pTab->coordRelativas[tempRel.x][tempRel.y] = 1;	
+
 }
+
 
 void borrarTablero (char dTab[][DIMTAB_Y], int coordX, int coordY){
 	
@@ -66,21 +91,31 @@ void borrarTablero (char dTab[][DIMTAB_Y], int coordX, int coordY){
 	}
 }
 
-//void imprimirBool ( _Bool bTab[][DIMTAB_REL]){
-//	
-//	int i, j;
-//	
-//	for (i = 0; i < 10; i++){
-//			
-//		for (j = 0; j<10; j++){
-//				
-//			printf("%d",bTab[i][j]);
-//		}
-//		
-//		printf("\n");
-//	}		
-//}
+void imprimir (char dTab [][DIMTAB_Y]){
+	
+	int i, j;
+	
+	for (i = 0; i < DIMTAB_X; i++){
+			
+		for (j = 0; j < DIMTAB_Y; j++){
+				
+			printf("%c",dTab[i][j]);
+		}
+	}
+	
+	printf("\n");	
+}
 
+coord toRel ( coord absolutas){
+	
+	coord temp;
+	
+	temp.x = (DIMTAB_REL - 1) - (ZFIL - absolutas.x)/ DFIL;
+	temp.y = (absolutas.y - ZCOL)/DCOL;
+	
+	return temp; 
+	
+}
 
 void getFicha(char dFicha[][DCOL], char dir[]){
 	
@@ -124,3 +159,227 @@ coord getCoordIni(int jug, int i){
 	return paso[i];
 
 }
+
+void saveGame(pieza *pJT, pieza *pJNT, pieza* pJ1, pieza*pJ2, int nPiezaJ1, int nPiezaJ2){
+	
+	int n, limite, i, j, turno, opcion;
+	_Bool sobrescribir = 1;
+	char aux, nombre[20];
+	partida listaPartidas[MAX_SAVE];
+	FILE *pf;
+		
+	n = leerPartidasGuardadas(pf, listaPartidas); //leerPartidasGuardadas.
+	
+	do{
+	
+		imprimirLista( listaPartidas, n, &limite, 1); //imprimir lista
+		opcion = seleccionarPartidaGuardada( limite, &n, &sobrescribir ); //seleccionar hueco de guardado.
+		
+	}while (!sobrescribir);
+	
+
+	if (opcion + 1 > n){
+		
+		printf ( "Elige un nombre para guardar la partida (max 20 caracteres):\n");
+		fflush(stdin);
+		scanf( "%20[^\n]",nombre);
+		strcpy(listaPartidas[opcion-1].nombre, nombre);
+	}
+	
+	if ( pJT == pJ1) turno = 1;
+	else turno = 2;
+ 
+	listaPartidas[opcion-1].turno = turno;
+	listaPartidas[opcion -1].nPiezJ1 = nPiezaJ1;
+	listaPartidas[opcion - 1].nPiezJ2 = nPiezaJ2;
+				
+	for ( i = 0; i < DIM_PIEZASJ; i++){
+		
+		listaPartidas[opcion-1].coordSaveJ1[i] = pJ1[i].cActual;  
+		listaPartidas[opcion-1].coordSaveJ2[i] = pJ2[i].cActual;
+	} 
+	
+	pf = fopen("partidasGuardadas.txt", "w");
+	if (pf==NULL) printf("No se puede acceder al fichero.\n");
+	
+	fprintf( pf, "%d\n", n );	
+	
+	for ( i = 0; i < n; i++){
+		
+		fprintf(pf, "%s;%d;%d;%d\n", listaPartidas[i].nombre, listaPartidas[i].turno, listaPartidas[i].nPiezJ1, listaPartidas[i].nPiezJ2);	
+			
+			for ( j = 0; j < DIM_PIEZASJ*2; j++){
+				
+				if ( j < DIM_PIEZASJ){
+					
+					fprintf(pf,"%d,%d\n", listaPartidas[i].coordSaveJ1[j].x, listaPartidas[i].coordSaveJ1[j].y);
+					
+				}else{
+					
+					fprintf(pf,"%d,%d\n", listaPartidas[i].coordSaveJ2[j - DIM_PIEZASJ].x, listaPartidas[i].coordSaveJ2[j - DIM_PIEZASJ].y);					
+				}
+			}
+	}
+
+	fclose(pf);
+}
+
+int leerPartidasGuardadas(FILE *pf, partida *listaPartidas){
+	
+	int i,j,r;
+	
+		pf = fopen("partidasGuardadas.txt", "r");
+	
+	fscanf(pf, "%d\n", &r);
+		
+	for( i = 0; i < r; i++ ){
+			
+		fscanf(pf, "%[^;];%d;%d;%d\n", listaPartidas[i].nombre, &listaPartidas[i].turno, &listaPartidas[i].nPiezJ1, &listaPartidas[i].nPiezJ2);
+			
+		for ( j = 0; j < DIM_PIEZASJ*2; j++){
+				
+				if ( j < DIM_PIEZASJ){
+					
+					fscanf(pf,"%d,%d\n", &listaPartidas[i].coordSaveJ1[j].x, &listaPartidas[i].coordSaveJ1[j].y);				
+				}else{
+					
+					fscanf(pf,"%d,%d\n", &listaPartidas[i].coordSaveJ2[j - DIM_PIEZASJ].x, &listaPartidas[i].coordSaveJ2[j - DIM_PIEZASJ].y);					
+				}
+			}	
+		}
+	
+	fclose(pf);
+	
+	return r;
+	
+}
+
+void imprimirLista( partida *listaPartidas, int n, int *limite, int tipo){
+	
+	int i;
+	
+	printf ( "Seleccione donde quiere guardar la partida:\n");
+	
+	for ( i = 0; i < n; i++){
+		
+		printf( "%d %s\n", i + 1, listaPartidas[i].nombre);
+	}
+	
+	if ( tipo == 1 ){
+	
+		if (n < MAX_SAVE){
+			 
+			printf("%d %s\n", i + 1, "Nuevos datos de guardado\n");
+			*limite = n + 1;
+			
+		}else *limite = n;
+	}
+}
+
+int seleccionarPartidaGuardada(int limite, int *n, _Bool *sobrescribir ){
+	
+	int opcion;
+	int ntemp = *n;
+	char aux;
+	
+	do {
+			
+		fflush(stdin); 	
+		scanf( "%d", &opcion);		
+
+	}while ( opcion < 1 || opcion > limite);
+	
+	if ( opcion <= ntemp){
+		
+		do{
+				
+			printf ("Hay una partida guardada en ese hueco. Desea sobrescribir la partida? (S/N)");
+			
+			fflush(stdin);
+			scanf( "%c", &aux);
+		}while ( aux != 'S' && aux != 'N');
+			
+		if ( aux == 'S') *sobrescribir = 1;
+		else *sobrescribir = 0;
+			
+	}else ntemp++;
+	
+	
+	*n = ntemp;
+		
+	return opcion;	
+}
+
+void loadGame (tab *pTablero, pieza *J1, pieza *J2, _Bool **load, int **turno, int **nJ1, int **nJ2, _Bool **cJuego  ){
+	
+	int n, limite, i, j, opcion;
+	partida listaPartidas[MAX_SAVE];
+	FILE *pf;	
+	
+	n = leerPartidasGuardadas(pf, listaPartidas); //leerPartidasGuardadas.	
+	
+	if ( n == 0){
+		
+		printf ("No hay partidas guardadas.\n");
+		**load = 0;
+		**cJuego = 0;
+		
+	}
+	else{
+		
+		**load = 1;
+		**cJuego = 1;
+		iniTablero(pTablero, DIRTAB);
+		
+		do{
+		
+			imprimirLista( listaPartidas, n, &limite, 2);		
+			scanf( "%d", &opcion);		
+
+		}while ( opcion < 1 || opcion > limite);
+	
+		// cargar variables.
+		
+		**nJ1 = listaPartidas[opcion - 1].nPiezJ1;
+		**nJ2 = listaPartidas[opcion - 1].nPiezJ2;
+		
+		for ( i = 0; i < DIM_PIEZASJ; i++){
+		
+			J1[i].cActual = listaPartidas[opcion - 1].coordSaveJ1[i];
+			if ( (J1[i].cActual.x != -1) && (J1[i].cActual.y != -1) ){
+				J1[i].relativas = toRel(J1[i].cActual);
+				getFicha(J1[i].dFigura, DIRPEONJ1);
+				setTablero( pTablero, J1[i].dFigura, J1[i].cActual.x, J1[i].cActual.y);				
+			
+			}
+			
+			J2[i].cActual = listaPartidas[opcion - 1].coordSaveJ2[i];	
+			if ( (J2[i].cActual.x != -1) && (J2[i].cActual.y != -1) ){				
+
+				J2[i].relativas = toRel(J2[i].cActual);	
+				getFicha(J2[i].dFigura, DIRPEONJ2);
+				setTablero( pTablero, J2[i].dFigura, J2[i].cActual.x, J2[i].cActual.y);	
+			}
+		}
+		
+		**turno = listaPartidas[opcion - 1].turno;	
+	} 
+	
+}
+
+
+
+//void imprimirBool ( _Bool bTab[][DIMTAB_REL]){
+//	
+//	int i, j;
+//	
+//	for (i = 0; i < 10; i++){
+//			
+//		for (j = 0; j<10; j++){
+//				
+//			printf("%d",bTab[i][j]);
+//		}
+//		
+//		printf("\n");
+//	}		
+//}
--- tablero_elementos.h
+++ tablero_elementos.h
@@ -1,17 +1,21 @@
 #include <stdio.h>
- 
-#define DIMTAB_X 63			//Dimensión del tablero(impresión).
+#include <string.h>
+
+#define DIMTAB_X 63		//Dimensión del tablero(impresión).
 #define DIMTAB_Y 146		//Dimensión del tablero(impresión).
 #define DIMTAB_REL 10		//Dimensión del tablero relativa a las casillas.
-#define DFIL 6 				//Desplazamiento de fila.
-#define DCOL 14 			//Desplazamiento de columnas.
-#define ZFIL 55 			//'Cero' de las filas.
-#define ZCOL 5				//'Cero' de las columnas.
-#define MFIL 7 				//Máximo de las filas.
-#define MCOL 117 			//Maximo de las columnas. 
+#define DFIL 6 			//Desplazamiento de fila.
+#define DCOL 14 		//Desplazamiento de columnas.
+#define ZFIL 55 		//'Cero' de las filas.
+#define ZCOL 5			//'Cero' de las columnas.
+#define MFIL 7 			//Máximo de las filas.
+#define MCOL 117 		//Maximo de las columnas. 
 #define DIM_PIEZASJ 8		//Dimensión arreglo de piezas.(8 piezas por jugador).
+#define MAX_SAVE 5
 
 #define DIRTAB "prTabV2.txt" //Dirección del archivo del tablero.(*INNECESARIO AQUÍ YA QUE SOLO LO LLAMAMOS UNA VEZ*)
+#include "fichas.h"
+
 
 typedef struct {
 	
@@ -21,29 +25,40 @@ typedef struct {
 
 typedef struct {
 	
-	int x;	//coordenada fila.
-	int y;	//coordenada columna.
+	int x;
+	int y;		
 }coord;
 
 typedef struct {
 	
-	char dFigura[5][DCOL];	//Dibujo de la pieza.
-//	jugadores pertenencia;
-//	int pertenencia;
-	coord cActual;			//Cordenadas absolutas(Dibujo).
-	coord relativas;		//Coordenadas relativas al tablero de juego.
+	char dFigura[5][DCOL];
+	coord cActual;
+	coord relativas;
 }pieza;
 
-void iniTablero(tab *pTablero, char[]);
+typedef struct {
+	
+	char nombre[21];
+	int turno;
+	coord coordSaveJ1[DIM_PIEZASJ];
+	coord coordSaveJ2[DIM_PIEZASJ];
+	int nPiezJ1;
+	int nPiezJ2;	
+}partida;
+
+void iniciarPartida(tab *pTablero, pieza*, pieza*, pieza*, pieza*, int, int *nJ1, int *nJ2, int *pnJ, char *mJ1, char *mJ2, char *mJ, _Bool *rCicloJuego );
+void iniTablero(tab *pTablero, char dir[]);
 void setTablero (tab *pTab, char ficha[][DCOL], int coordX, int coordY);
-void imprimir (char dTab[][DIMTAB_Y]);
-void borrarTablero (char [][DIMTAB_Y], int, int);
-int toRelX ( int );
-int toRelY ( int );
-
-void getFicha(char [][DCOL], char[]);
-coord getCoordIni(int, int);
-
+void borrarTablero (char dTab[][DIMTAB_Y], int coordX, int coordY);
+void imprimir (char dTab [][DIMTAB_Y]);
+coord toRel ( coord absolutas);
+void getFicha(char dFicha[][DCOL], char dir[]);
+coord getCoordIni(int jug, int i);
+void saveGame(pieza *pJT, pieza *pJNT, pieza *pJ1, pieza *pJ2, int nPiezaJ1, int nPiezaJ2);
+int leerPartidasGuardadas(FILE *pf, partida *listaPartidas);
+void imprimirLista( partida *listaPartidas, int n, int *limite, int tipo);
+int seleccionarPartidaGuardada(int limite, int *n, _Bool *sobrescribir );
+void loadGame (tab *pTablero, pieza *J1, pieza *J2, _Bool **load, int **turno, int **nJ1, int **nJ2, _Bool **cJuego );
 //codigo de prueba 
 //void imprimirBool (_Bool [][DIMTAB_REL] ); 
 
